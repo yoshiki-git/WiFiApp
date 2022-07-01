5d1426bb6bc5fba2ad57dc0c2d04dc55762247ad
Yoshiki-Sato（NTC） <sato.yoshiki.32@ntc.co.jp>
2022-07-01 17:05:16 +0900

    YahooMailを用いてメール送信可能にした

--- app/src/main/java/com/example/wifiapp/SendMailService.kt
+++ app/src/main/java/com/example/wifiapp/SendMailService.kt
@@ -19,6 +19,21 @@ import javax.mail.internet.MimeMessage
 import javax.mail.internet.MimeMultipart
 import kotlin.concurrent.thread
 
+
+/*
+受信メール（IMAP）サーバー	imap.mail.yahoo.co.jp
+受信メール（IMAP）通信方法	SSL
+受信メール（IMAP）ポート番号	993
+送信メール（SMTP）サーバー	smtp.mail.yahoo.co.jp
+送信メール（SMTP）認証方式	SMTP_AUTH
+送信メール（SMTP）通信方法	SSL
+送信メール（SMTP）ポート番号	465
+アカウント名/ログイン名	Yahoo! JAPAN ID
+※ログイン時にシークレットIDを使用する設定にしている場合は、シークレットIDを入力してください。
+メールアドレス	zrnuruwf@yahoo.co.jp
+パスワード	Yoshiki32@ntct
+ */
+
 class SendMailService(val fromUsername:String,val password:String,val toAddress:String,val filePath:String,val fileName:String){
     private val TAG = "SendMailService.kt"
 
@@ -30,18 +45,36 @@ class SendMailService(val fromUsername:String,val password:String,val toAddress:
         Log.d(TAG,filePath)
         Log.d(TAG,fileName)
 
+        //YahooMail
+        //zrnuruwf@yahoo.co.jp
+        //Yoshiki32@ntct
+
+
         val property = Properties()
         //SMTPを用いる IMAPとPOP3もいけるらしい
+        //Yahooメール用
+        property.put("mail.smtp.host", "smtp.mail.yahoo.co.jp") //メールホストのサーバ
+        property.put("mail.smtp.auth", "true") //AUTHコマンドを使用してユーザ認証を試みる
+        property.put("mail.transport.protocol","smtp")
+        property.put("mail.smtp.ssl.enable","true") //SSLで通信するときにいる
+        property.put("mail.smtp.port", "465") //接続するSMTPサーバーポート 465? 587?
+        property.put("mail.smtp.debug", "true")
+
+        /*
+        Gmail 2022.5.30より使用不可になった
         property.put("mail.smtp.host", "smtp.gmail.com") //メールホストのサーバー　今回はgmailにしている
         property.put("mail.smtp.auth", "true") //AUTHコマンドを使用してユーザ認証を試みる
         property.put("mail.smtp.starttls.enable", "true") //TLSで通信？
         property.put("mail.smtp.port", "587") //接続するSMTPサーバーポート
         property.put("mail.smtp.debug", "true")
 
+         */
+
         val message: Message = MimeMessage(Session.getDefaultInstance(property, object: Authenticator() {
             override fun getPasswordAuthentication(): PasswordAuthentication {
                 //送信元アドレスとパスワードを引数に
-                return PasswordAuthentication(fromUsername, password)
+                return PasswordAuthentication("zrnuruwf@yahoo.co.jp", "Yoshiki32@ntct")
+               // return PasswordAuthentication(fromUsername, password)
             }
         }))
 
@@ -64,7 +97,7 @@ class SendMailService(val fromUsername:String,val password:String,val toAddress:
 
         try{
             //送信元を引数に
-            message.setFrom(InternetAddress(fromUsername, "Fromの表示名です"))
+            message.setFrom(InternetAddress("zrnuruwf@yahoo.co.jp", "Fromの表示名です"))
             //送信先を引数に
             message.setRecipient(Message.RecipientType.TO, InternetAddress(toAddress, "Toの表示名です"))
             message.subject = "Wifiログアプリ自動送信メール"
